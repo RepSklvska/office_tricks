269ba19b71a9d988618735951177a87214e07dab
Alexander Sokolov <gunpowderfans@mail.ru>
2020-12-21 06:53:11 +0800

    Create xlsx_to_list.py

---
+++ xlsx_to_list.py
@@ -0,0 +1,12 @@
+def xlsx_to_list(filename):
+	import pandas
+	import xlsxwriter
+	origin_frame = pandas.read_excel(filename)
+	columns = origin_frame.columns.to_list()
+	list_from_xlsx = [columns]
+	for i in origin_frame.index:
+		line = []
+		for ii in origin_frame.loc[i].values.tolist():
+			line.append(str(ii).strip())
+		list_from_xlsx.append(line)
+	return list_from_xlsx
